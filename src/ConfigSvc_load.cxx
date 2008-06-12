69b69c18d78bf23813bf05a384f678ec88fc3aeb
echarles <>
2008-06-12 02:02:32 +0000

    adding ConfigSvc

---
+++ src/ConfigSvc_load.cxx
@@ -0,0 +1,14 @@
+/** 
+* @file Trigger_load.cxx
+* @brief This is needed for forcing the linker to load all components
+* of the library.
+*
+*  $Header: /nfs/slac/g/glast/ground/cvs/Trigger/src/Trigger_load.cxx,v 1.9 2007/09/05 16:43:38 heather Exp $
+*/
+
+#include "GaudiKernel/DeclareFactoryEntries.h"
+
+DECLARE_FACTORY_ENTRIES(ConfigSvc) {
+    DECLARE_SERVICE( ConfigSvc );
+} 
+
