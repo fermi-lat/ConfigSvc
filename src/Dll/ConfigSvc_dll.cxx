f8d4cc081987f08906898d3400b6bde8afdd5a2c
heather <>
2008-06-19 17:33:55 +0000

    use component library pattern should fix win build

---
+++ src/Dll/ConfigSvc_dll.cxx
@@ -0,0 +1,8 @@
+/** @file Trigger_dll.cxx
+
+  $Header: /nfs/slac/g/glast/ground/cvs/ConfigSvc/src/ConfigSvc_dll.cxx,v 1.1.1.1 2008/06/12 02:02:32 echarles Exp $
+*/
+
+#include "GaudiKernel/LoadFactoryEntries.h"
+LOAD_FACTORY_ENTRIES(ConfigSvc)
+
--- src/Dll/ConfigSvc_load.cxx
+++ src/Dll/ConfigSvc_load.cxx
@@ -3,7 +3,7 @@
 * @brief This is needed for forcing the linker to load all components
 * of the library.
 *
-*  $Header: /nfs/slac/g/glast/ground/cvs/Trigger/src/Trigger_load.cxx,v 1.9 2007/09/05 16:43:38 heather Exp $
+*  $Header: /nfs/slac/g/glast/ground/cvs/ConfigSvc/src/ConfigSvc_load.cxx,v 1.1.1.1 2008/06/12 02:02:32 echarles Exp $
 */
 
 #include "GaudiKernel/DeclareFactoryEntries.h"
