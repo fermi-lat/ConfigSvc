870361f7943a47ed86ad9c1c27eae37aca36e663
jrb <>
2012-08-17 18:41:02 +0000

    patches for Windows no-install-headers

--- ConfigSvcLib.py
+++ ConfigSvcLib.py
@@ -1,13 +1,17 @@
-#$Header: /nfs/slac/g/glast/ground/cvs/GlastRelease-scons/ConfigSvc/ConfigSvcLib.py,v 1.1 2008/08/15 21:42:34 ecephas Exp $
+#$Header: /nfs/slac/g/glast/ground/cvs/GlastRelease-scons/ConfigSvc/ConfigSvcLib.py,v 1.2 2009/11/10 01:56:22 jrb Exp $
 def generate(env, **kw):
     if not kw.get('depsOnly', 0):
         env.Tool('addLibrary', library = ['ConfigSvc'])
+        if env['PLATFORM']=='win32' and env.get('CONTAINERNAME','')=='GlastRelease':
+	    env.Tool('findPkgPath', package = 'ConfigSvc') 
 
     env.Tool('configDataLib')
     env.Tool('MootSvcLib')
     env.Tool('EventLib')
     env.Tool('LdfEventLib')
     env.Tool('addLibrary', library = env['gaudiLibs'])
+    if env['PLATFORM']=='win32' and env.get('CONTAINERNAME','')=='GlastRelease':
+        env.Tool('findPkgPath', package = 'enums') 
 
 def exists(env):
     return 1;
